77291fbfdabacd2085cbbcd36a6c79bd9fc0cc9e
alexwmsoft <alex.wmsoft@outlook.com>
2020-07-06 22:13:10 +0700

    profile.js added

---
+++ profile.js
@@ -0,0 +1,57 @@
+Thanks for visiting my profile.
+I'm a talented program developer with many detailed knowledge of how to build a perfect application.
+I am a Full stack Web Developer with 9+ years of experiences working on the website.
+
+My skills:
+
+1. Front End
+  - HTML5/CSS3, JavaScript/jQuery, Sass/Scss/Less
+  - Bootstrap, Bulma, Semantic UI, Responsive Web
+  - ES6, React(Redux, Flux), Angular(RxJS), Vue.js/Quasar
+  - PSD to HTML, XD to HTML, Figma to HTML
+  - Gulp
+
+2. Back end
+  - PHP 7, Laravel, CodeIgniter, Yii
+  - Java, Spring, Struts, Hibernate
+  - Python, Django, Flask
+  - Node.js, ExpressJS
+  - Restful APIs
+  - ASP.NET, C#
+  - MySQL, MariaDB, SQLite, MSSQL, PostgreSQL, MongoDB, Redis
+
+3. API development and integration
+  - Payment gateways: PayPal, Stripe, Uphold
+  - Google APIs, Twilio, Zapier
+  - SNS API: Facebook, Twitter, Youtube, Yelp
+  - Google map API
+
+4. Mobile
+  - React Native
+  - Ionic
+
+5. Other
+  - High Quality
+  - Clean Coding
+  - Perfect Result
+
+Previous Works:
+- https://lykkeboks.net
+- https://pondera.fr
+- http://accompagnement.pondera.fr
+- http://office.pondera.fr
+- http://driveus.ca
+- https://www.804meter.com
+- https://otomobiru.com
+- https://sghomeneeds.com
+- https://kushbitx.com
+- https://foodypartner.com
+- https://www.worldtalentjobs.com/
+- https://www.careermonitor.com.au
+- http://www.ezdocpro.com
+- https://gohire.io
+- https://www.viacharacter.org
+
+Skype: jnx_teamwork@outlook.com
+I want to work safely using skype.
+Will wait your connection.
